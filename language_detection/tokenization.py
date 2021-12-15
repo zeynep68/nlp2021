bbfc99941d02fdcfdcc55165ad64b1fbfd0679e5
zeynep068 <zeynep.boztoprak@hhu.de>
2021-12-16 00:42:00 +0100

    refactor: change way of splitting

--- language_detection/tokenization.py
+++ language_detection/tokenization.py
@@ -1,94 +1,23 @@
-import numpy as np
-
-NUMBERS = [str(0), str(1), str(2), str(3), str(4), str(5), str(6), str(7),
-           str(8), str(9)]
-
-def split(tokens, t, c1, c2=None):
-    a = t.split(c1)
-    a.insert(-1, c1)
-
-    if c2 is not None:
-        a.insert(-1, c2)
-        a[-1] = a[-1].replace("'", '')
-    while '' in a:
-        a.remove('')
-    if 'http:' in t:
-        del a[-1]
-
-    tokens += a
-
-    return tokens
+chars = [', ', '$', '?', '(', ')', '"', '—', '!', ';', "''", '%', ".'"]
 
 
 def tokenize(s):
     tokens = []
 
-    for t in s.split(' '):
-        if "\n" in t:
-            t = t.replace("\n", "")
-        
-        if '.' in t:
-            if t.startswith('$'):
-                tokens.append(t[0])
-                t = t[1:]
+    for c in chars:
+        s = s.replace(c, ' ' + c + ' ')
 
-            idx = t.index('.')
-            if '[' in t:
-                tokens = split(tokens, t, '[')
-            
-            elif (idx+1 < len(t)) and (t[idx+1] in NUMBERS):
-                tokens.append(t[:-1])
-                tokens.append('.')
-
-            elif ".'" in t:
-                tokens = split(tokens, t, '.', "'") 
-                
-            elif ')' in t:
-                tokens = split(tokens, t, ')') 
-
-            else:
-                tokens = split(tokens, t, '.') 
-
-        elif ',' in t:
-            if (t.endswith(',')) and ('(' not in t):
-                tokens = split(tokens, t, ',')
-
-            elif '$' in t:
-                tokens = split(tokens, t, '$')
-
-            elif '(' in t:
-                tokens = split(tokens, t, t[t.index('(')+1])
-            else:
-                tokens.append(t)
-
-        elif ';' in t:
-            tokens = split(tokens, t, ';') 
-
-        elif '?' in t:
-            tokens = split(tokens, t, '?') 
-
-        elif "''" in t:
-            tokens = split(tokens, t, "''") 
-
-        elif '%' in t:
-            tokens = split(tokens, t, '%') 
-
-        elif '"' in t:
-            tokens = split(tokens, t, '"') 
-
-        elif '!' in t:
-            tokens = split(tokens, t, '!') 
-
-        elif ')' in t:
-            tokens = split(tokens, t, ')') 
-
-        elif '(' in t:
-            tokens = split(tokens, t, '(') 
-
-        elif '—' in t:
-            tokens = split(tokens, t, '—') 
-
+    # special cases
+    for t in s.split(): 
+        if '[http' in t:
+            tokens.append(t[0])
+        elif t == ".'":
+            tokens.append(t[0])
+            tokens.append(t[1])
+        elif t.endswith('.'):
+            tokens.append(t[:-1])
+            tokens.append(t[-1])
         else:
             tokens.append(t)
-
-    return tokens
+   
+    return [t for t in tokens if t != '']
